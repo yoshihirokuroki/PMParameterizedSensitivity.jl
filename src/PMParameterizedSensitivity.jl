60fdf6e79b2a0e24a02235a3439d8350d89ebe35
Timothy Knab <knabt@metrumrg.com>
2023-10-29 19:49:35 -0400

    Fixes

--- src/PMParameterizedSensitivity.jl
+++ src/PMParameterizedSensitivity.jl
@@ -3,6 +3,7 @@ module PMParameterizedSensitivity
     using Reexport
     using PMParameterizedBase
     import PMSimulatorBase: PMEvent
+    import PMSimulatorSimulate: collect_evs
     @reexport using SciMLSensitivity
     import SciMLSensitivity: solve
     PMModel = PMParameterizedBase.PMModel
