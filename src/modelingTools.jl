656c833de3749150efde9589bc180d87d0aa5451
Tim Knab <knabt@metrumrg.com>
2023-10-29 22:54:05 -0400

    include inputs in parameters

--- src/modelingTools.jl
+++ src/modelingTools.jl
@@ -1,22 +1,30 @@
+
 function SciMLSensitivity.ODEForwardSensitivityProblem(mdl::PMModel, sensealg::SciMLSensitivity.AbstractForwardSensitivityAlgorithm = ForwardSensitivity();
     kwargs... )
     f = mdl._odeproblem.f
     parpairs = mdl.parameters.values
     conpairs = mdl.constants.values
     varpairs = mdl.states.values
+    inpairs = mdl._inputs.values
 
     pars = [p.first for p in parpairs]
     cons = [c.first for c in conpairs]
     vars = [v.first for v in varpairs]
+    ins = [i.first for i in inpairs]
 
-    pin = ModelingToolkit.varmap_to_vars(vcat(parpairs,conpairs), vcat(pars, cons))[1:lastindex(pars)]
-    u0in = ModelingToolkit.varmap_to_vars(vcat(varpairs, parpairs,conpairs), vcat(vars, pars, cons))[1:lastindex(vars)]
+    # pin = ModelingToolkit.varmap_to_vars(vcat(parpairs,conpairs), vcat(pars, cons))[1:lastindex(pars)]
+    # u0in = ModelingToolkit.varmap_to_vars(vcat(varpairs, parpairs,conpairs), vcat(vars, pars, cons))[1:lastindex(vars)]
+    pin = ModelingToolkit.varmap_to_vars(vcat(parpairs,inpairs, conpairs), vcat(pars, ins, cons))[1:(lastindex(pars)+lastindex(ins))]
+    u0in = ModelingToolkit.varmap_to_vars(vcat(varpairs, parpairs, conpairs), vcat(vars, pars, cons))[1:lastindex(vars)]
 
     tspan = mdl.tspan
     sens_prob = SciMLSensitivity.ODEForwardSensitivityProblem(f, u0in, tspan, pin, sensealg; kwargs...)
     return sens_prob
 end
 
+
+
+function SciMLSensitivity.solve
 
 
 # function SciMLSensitivity.ODEForwardSensitivityProblem(mdl_in::PMModel, evs::Vector{PMEvent}, sensealg::SciMLSensitivity.AbstractForwardSensitivityAlgorithm = ForwardSensitivity();
