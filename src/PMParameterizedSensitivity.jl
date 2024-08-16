00d418549441d5efd2b7b31c7158baa96b913023
Yoshihiro Kuroki <yoshihiro.kuroki@gmail.com>
2024-08-16 23:43:18 +0900

    modifying the paths to the dependent modules

--- src/PMParameterizedSensitivity.jl
+++ src/PMParameterizedSensitivity.jl
@@ -1,10 +1,10 @@
 
 module PMParameterizedSensitivity
     using Reexport
-    using PMParameterizedBase
-    import PMSimulatorBase.PMEvent
-    import PMSimulatorBase.collect_evs
-    using PMParameterizedSolve
+    using ..PMParameterizedBase
+    import ..PMSimulatorBase.PMEvent
+    import ..PMSimulatorBase.collect_evs
+    using ..PMParameterizedSolve
     import DiffEqCallbacks: CallbackSet
     using SciMLSensitivity
     PMModel = PMParameterizedBase.PMModel
